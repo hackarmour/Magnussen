2fe520bb550cc31ced4ab3c5b9b85f2f470b5d62
Gurpal Singh <gillgurpal699@gmail.com>
2023-06-06 07:33:38 +0000

    remove for loop

--- node/test.js
+++ node/test.js
@@ -20,32 +20,10 @@ const getKeyResponse = await client.sendCommand("get", "mykey");
 const echoResponse = await client.sendCommand("echo", "hello world");
 
 const deleteKeyResponse = await client.sendCommand("del", "mykey");
-// console.log({
-//   set: setKeyResponse,
-//   ping: pingResponse,
-//   get: JSON.parse(getKeyResponse),
-//   echo: echoResponse,
-//   del: deleteKeyResponse,
-// });
-
-console.time("test");
-let i = 0;
-const starting = `Starting: Memory usage: ${
-  process.memoryUsage().heapUsed / 1024 / 1024
-} MB`;
-for (const _ of Array(10000)) {
-  i += 1;
-  await client.sendCommand(
-    "set",
-    "mykey",
-    JSON.stringify({
-      message: "segs",
-    })
-  );
-  await client.sendCommand("get", "mykey");
-  await client.sendCommand("del", "mykey");
-  console.log(`Done ${i}`);
-}
-console.timeEnd("test");
-console.log(starting);
-console.log(`Ending: Memory usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB`);
+console.log({
+  set: setKeyResponse,
+  ping: pingResponse,
+  get: JSON.parse(getKeyResponse),
+  echo: echoResponse,
+  del: deleteKeyResponse,
+});
